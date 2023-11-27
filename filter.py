fdae30500c4eb23bf22b564cb9acaa147b7a972e
TonyP4N <92439213+TonyP4N@users.noreply.github.com>
2023-11-27 22:56:18 +0000

    Add new train dataset and do data pruning

---
+++ filter.py
@@ -0,0 +1,22 @@
+import os.path
+import re
+
+documents = []
+
+path = os.path.realpath("train_data")
+files = os.listdir(path)
+print(files)
+
+for file in files:
+    with open(f'train_data/{file}', "r") as f:
+        document = ""
+        for line in f:
+            if re.search("SZ\[13\]", line):
+                document += line
+        if document:
+            documents.append(document)
+
+with open("train_dataset_test", "w") as f:
+    for doc in documents:
+        f.write(doc + "\n")
+
