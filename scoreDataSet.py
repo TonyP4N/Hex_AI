9cc302b310ade7557d76085e2d643b0e6afec625
Qchensan <1582471167@qq.com>
2023-12-13 21:17:11 +0000

    new mentor

--- scoreDataSet.py
+++ scoreDataSet.py
@@ -2,7 +2,22 @@ import numpy as np
 from resistance import score
 from preprocess import *
 
-positions = preprocess("train_data/raw_games.dat")
+# result = []
+# with open("train_data/training_data.txt", 'r') as f:
+# 	for line in f:
+# 		line = line.strip()
+# 		line = line.split(" ")
+# 		if line[0] == line[1]:
+# 			result.append(' '.join(line[1:]))
+# 		else:
+# 			result.append(' '.join(line))
+
+# print(result)
+# with open("train_data/training_data.dat", 'w') as f:
+# 	for line in result:
+# 		f.write(line + '\n')
+
+positions = preprocess("train_data/training_data.dat")
 print ("scoring positions...")
 scores = np.empty((positions.shape[0],boardsize,boardsize))
 num_positions = positions.shape[0]
@@ -18,5 +33,5 @@ for i in range(num_positions):
 		i-=1
 
 print ("saving to file...")
-savefile = open("train_data/scoredPositionsFull.npz", 'wb')
+savefile = open("train_data/scoredPositionsFull2.npz", 'wb')
 np.savez(savefile, positions=positions, scores=scores)
